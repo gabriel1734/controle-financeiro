08332a938792e5716c7df14a659838590623f7c5
Crispim14 <jcrispim00@gmail.com>
2024-07-02 20:48:02 -0300

    :card_file_box: Adicão das Migrition, Models, Context

---
+++ Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace controlefinanceiro.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+    }
+}
---
+++ Models/Transacao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controlefinanceiro.Models
+{
+    public class Transacao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public Usuario Usuario { get; set; }
+
+        [Required]
+        public int CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        public string Descricao { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Tipo { get; set; }
+    }
+}
