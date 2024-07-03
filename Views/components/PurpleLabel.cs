92df6a90f4b2db75133e1512ea427fa22fbe0f3c
Crispim14 <132013854+Crispim14@users.noreply.github.com>
2024-07-02 21:58:13 -0300

    1 teste

---
+++ Views/components/PurpleLabel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeusControles
+{
+    public class PurpleLabel : Label
+    {
+        public PurpleLabel()
+        {
+            this.ForeColor = Color.FromArgb(149, 88, 225);
+            this.Font = new Font("Consolas", 14F, FontStyle.Bold);
+        }
+    }
+}
